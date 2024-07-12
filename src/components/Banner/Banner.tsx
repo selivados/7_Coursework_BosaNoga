5279d296d41a2649d72f31eb6985aa1f3a92ac9a
Anton <freelanset@gmail.com>
2024-07-12 16:45:17 +0800

    Changed image src in Banner and Logo components

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,11 +1,12 @@
 import { FC } from "react";
 
+import banner from "../../img/banner.jpg";
 import "./banner.css";
 
 export const Banner: FC = () => {
   return (
     <div className="banner">
-      <img src="/src/img/banner.jpg" className="img-fluid" alt="К весне готовы!"/>
+      <img src={banner} className="img-fluid" alt="К весне готовы!"/>
       <h2 className="banner-header">К весне готовы!</h2>
     </div>
   );
--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,10 +1,12 @@
 import { FC } from "react";
 import { Link } from "react-router-dom";
 
+import logo from "../../img/header-logo.png";
+
 export const Logo: FC = () => {
   return (
     <Link className="navbar-brand" to="/">
-      <img src="/src/img/header-logo.png" alt="Bosa Noga" />
+      <img src={logo} alt="Bosa Noga" />
     </Link>
   );
 };
